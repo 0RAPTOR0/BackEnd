f1db3bd0c70c51b9ecb33f0392f7b7543b67e551
0RAPTOR0 <xxiiraptor@gmail.com>
2024-06-19 20:28:19 +0530

    update and delete in CRED

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const Model = require('../models/userModel');
+const Model = require('../models/usermodel');
 
 const router = express.Router();
 
@@ -76,6 +76,31 @@ router.get('/getbyid/:id', (req, res) => {
 
         });
 })
+
+router.put('/update/:id',(req,res) => {
+    Model.findByIdAndUpdate(req.params.id, req.body, { new : true })
+        .then((result) => {
+            res.status(200).json(result)
+
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err)
+
+        });
+});
+
+router.delete('/delete/:id', (req, res) => {
+    Model.findByIdAndDelete(req.params.id)
+        .then((result) => {
+            res.status(200).json(result)
+
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err)
+
+        });
+})
+
 
 
 
