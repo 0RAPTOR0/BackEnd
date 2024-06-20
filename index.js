5e740d46d992f1a731864f9504b2e66416553666
0RAPTOR0 <xxiiraptor@gmail.com>
2024-06-20 20:31:31 +0530

    connection t db

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ const express = require('express');
 const UserRouter = require('./routers/userRouter')
 const FeedRouter = require('./routers/feedRouter')
 
+const cors = require('cors');
+
 // initialize express
 const app = express();
 
 const port = 5000;     //server port
 
 // middleware
+
+// handling cors
+app.use(cors({
+    origin:['http://localhost:3000']
+}));
 
 app.use(express.json());                       //data goes from this and this converts jason or other to java script
 
